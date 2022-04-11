f6dc5c295fe1a553d3d1c882566bb013d6e33042
dizenas <103389312+dizenas@users.noreply.github.com>
2022-04-11 21:00:41 +0300

    Add files via upload

---
+++ WpfAppName/WpfAppName/Helper/FindRole.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using WpfAppName.Model;
+using WpfAppName.ViewModel;
+namespace WpfAppName.Helper
+{
+    public class FindRole
+    {
+        int id;
+        public FindRole(int id)
+        {
+            this.id = id;
+        }
+        public bool RolePredicate(Role role)
+        {
+            return role.Id == id;
+        }
+    }
+}
---
+++ WpfAppName/WpfAppName/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using WpfAppName.View;
+
+namespace WpfAppName
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    
+    
+    public partial class MainWindow : Window
+    {
+        public static int IdRole { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Employee_OnClick(object sender, RoutedEventArgs e)
+        {
+            WindowEployee wEmployee = new WindowEployee();
+            wEmployee.Show();
+        }
+        private void Role_OnClick(object sender, RoutedEventArgs e)
+        {
+            WindowRole wRole = new WindowRole();
+            wRole.Show();
+        }
+    }
+}
